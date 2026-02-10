c80b1b0ebe25a6a2bae50b17801e00485e07e37d
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-02-10 21:59:50 +0100

    Creamos los objetos para poder hacer funcionar bien el juego

--- src/Dragrones_Y_Mazmorras/ejecucion.java
+++ src/Dragrones_Y_Mazmorras/ejecucion.java
@@ -19,6 +19,30 @@ public class ejecucion {
     Ladron ladron = new Ladron("Robin", 4, 80);
     
     System.out.println("¡Personajes creados!\n");
+
+	// ===== MOSTRAR INFORMACIÓN =====
+    System.out.println("--- Información de los personajes ---\n");
+    mago.mostrarInfo();
+    guerrero.mostrarInfo();
+    ladron.mostrarInfo();
+    
+    
+    // ===== AÑADIR EQUIPAMIENTO =====
+    System.out.println("\n--- Añadiendo equipamiento ---\n");
+    
+    // Crear objetos de equipamiento
+    equipamento baston = new equipamento("Bastón Mágico", "arma");
+    equipamento tunica = new equipamento("Túnica Azul", "armadura");
+    equipamento espada = new equipamento("Espada de Acero", "arma");
+    equipamento daga = new equipamento("Daga Sigilosa", "arma");
+    
+    // Añadir equipamiento a los personajes
+    mago.agregarEquipamiento(baston);
+    mago.agregarEquipamiento(tunica);
+    guerrero.agregarEquipamiento(espada);
+    ladron.agregarEquipamiento(daga);
+    
+    System.out.println();
     
     
 
