d36d77ed48da65ed94fe5b36219536f378592ded
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-02-10 22:16:31 +0100

    Metodo para dar datos del guerrero

--- src/Dragrones_Y_Mazmorras/Guerrero.java
+++ src/Dragrones_Y_Mazmorras/Guerrero.java
@@ -30,3 +30,12 @@ public void dejarDeProteger() {
 public Personaje estaProtegiendo() {
 return companeroProtegido;
 }
+                       public void mostrarInfo() {
+    super.mostrarInfo();
+    if (companeroProtegido != null) {
+        System.out.println("Protegiendo a: " + companeroProtegido.getNombre());
+    } else {
+        System.out.println("No está protegiendo a nadie");
+    }
+    System.out.println("======================");
+}
