216c8fbb03d0bf266f25bfa8e234d8b5cd169515
jairo061999-prog <jairo061999@alumnes.ilerna.com>
2026-02-11 18:46:49 +0100

    Añadimos atributos como nombre, nivel, puntos de vida, inventario y la proteccion
     Crea un nuevo personaje, se crea un inventario vacio

--- src/Dragrones_Y_Mazmorras/Personaje.java
+++ src/Dragrones_Y_Mazmorras/Personaje.java
@@ -4,5 +4,12 @@ Public class Personaje{
   Protected int puntosVida;
   Protected boolean protegido;
   Protected inventario inventario;
-  
+
+  Public Personaje(String nombre,int nivel, int PuntosVida)
+  this.nombre = nombre;
+  this.nivel = nivel;
+  this.puntosVida = puntosVida;
+  this.protegido = false;
+  this.inventario = new inventario();
+
   
