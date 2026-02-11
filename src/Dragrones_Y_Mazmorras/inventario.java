8762eff262a255c1038ea6667567d93ce15b99c2
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-02-11 01:18:59 +0100

    Esta funcion muestra el contenido del inventario y avisa si esta vacio

--- src/Dragrones_Y_Mazmorras/inventario.java
+++ src/Dragrones_Y_Mazmorras/inventario.java
@@ -9,3 +9,14 @@ public void agregarEquipamiento(equipamento equipo) {
 equipamiento.add(equipo);
 System.out.println("Se ha añadido: " + equipo.getNombre());
 }
+public void mostrarInventario() {
+System.out.println("=== INVENTARIO ===");
+if (equipamiento.isEmpty()) {
+System.out.println("El inventario está vacío");
+    } else {
+        for (equipamento equipo : equipamiento) {
+            System.out.println("- " + equipo.getNombre() + " (" + equipo.getTipo() + ")");
+        }
+    }
+    System.out.println("==================");
+}
