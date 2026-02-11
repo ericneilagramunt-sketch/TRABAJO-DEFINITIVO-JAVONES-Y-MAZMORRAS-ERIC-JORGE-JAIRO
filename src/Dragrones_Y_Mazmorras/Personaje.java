5e125a8944dbdcef6a1b7d032a2f085ce7ad6e0a
jairo061999-prog <jairo061999@alumnes.ilerna.com>
2026-02-11 17:10:23 +0100

    Creamos los atributos protegidos
    (Seran la clase hija)

--- src/Dragrones_Y_Mazmorras/Personaje.java
+++ src/Dragrones_Y_Mazmorras/Personaje.java
@@ -1,1 +1,8 @@
-
+Public class Personaje{
+  Protected String nombre;
+  Protected int nivel;
+  Protected int puntosVida;
+  Protected boolean protegido;
+  Protected inventario inventario;
+  
+  
